c6c46d7484d04e220aab761f34ad101b8ad9d255
Brianyuko <brianyuko45@gmail.com>
2020-06-16 02:55:25 +0700

    Delete Data

---
+++ DeleteData.py
@@ -0,0 +1,21 @@
+from peewee import *
+
+sqlite_db = SqliteDatabase('login.db')
+
+
+class Login(Model):
+    username = CharField()
+    point = IntegerField()
+
+    class Meta:
+        database = sqlite_db
+
+
+sqlite_db.connect()
+
+# Delete Data Option One
+# user = Login.get(Login.id == 4)
+# user.delete_instance()
+
+# Delete Data Option Two
+Login.delete().where(Login.point < 3).execute()
